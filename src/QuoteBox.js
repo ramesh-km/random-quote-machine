d79edb621472276d0430cbc467b4462568b71b2b
Ramesh KM <ramesh-km@outlook.com>
2020-07-31 21:46:50 +0530

    no internet show img & loader

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,51 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../src/app.module.scss';
+import noInternet from './noInternet.png';
 
 function QuoteBox() {
-    const [quotes, setQuotes]= useState([{text: 'What is the best thing I can do right now ?', author: 'rk'}]);
+    const [quotes, setQuotes]= useState([{text: '', author: ''}]);
     const [randomNumber, setRandomNumber] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetchQuotes().then(value => {
-            setQuotes(value);
-            setRandomNumber(getRandomNumber(value.length));
-            document.body.style.backgroundColor = getRandomColor(0.5);
-            document.getElementById('quote-box').style.backgroundColor = getRandomColor(0.3) ;
-        });
+        if (window.navigator.onLine) {
+            fetchQuotes().then(value => {
+                setQuotes(value); 
+                setRandomNumber(getRandomNumber(value.length));
+                setIsLoading(false);
+            });
+        }
     }, []);
+
+    useEffect(() => {
+        document.body.style.backgroundColor = getRandomColor(0.5);
+        if (!isLoading) {
+            document.getElementById('quote-box').style.backgroundColor = getRandomColor(0.3);
+        }
+    });
 
     function onNewQuoteClick(event) {
         setRandomNumber(getRandomNumber(quotes.length));
-        document.body.style.backgroundColor = getRandomColor(0.5);
-        document.getElementById('quote-box').style.backgroundColor = getRandomColor(0.3);
     }
 
     const quote = quotes[randomNumber];
     const twitterIntentUrl = `https://twitter.com/intent/tweet?hashtags=quote&related=freecodecamp&text=${encodeURI(quote.text + '\n' + quote.author)}`;
+
+    if (!window.navigator.onLine) {
+        return (<div className={styles.noInternet} >
+            <img src={noInternet} alt='Illustration for no internet connectivity'></img>
+            <p>No internet ?</p>
+        </div>);
+    }else if (isLoading) {
+        return (<div className={styles.loader}>Loading...</div>)
+    }
+
     return (
-        <section id='quote-box'>
-            <Quote quote={quote}/>
-            <div id='quote-box-footer'>
-                <div className='social'>
-                    <a id='tweet-quote'
-                        href={twitterIntentUrl}
-                        className='fa fa-twitter'
-                        title='twitterIcon'
-                        target='blank'
-                    >
-                    </a>
-                </div>
-                <button
-                    type='button'
-                    onClick={onNewQuoteClick}
-                    id='new-quote'
-                    className={styles.buttonOn}
+    <section id='quote-box'>
+        <Quote quote={quote}/>
+        <div id='quote-box-footer'>
+            <div className='social'>
+                <a id='tweet-quote'
+                    href={twitterIntentUrl}
+                    className='fa fa-twitter'
+                    title='twitterIcon'
+                    target='blank'
                 >
-                    New Quote
-                </button>
+                </a>
             </div>
-        </section>
-    )
+            <button
+                type='button'
+                onClick={onNewQuoteClick}
+                id='new-quote'
+                className={styles.buttonOn}
+            >
+                New Quote
+            </button>
+        </div>
+    </section>
+    );
 }
 
 function Quote({quote}) {
@@ -57,20 +75,13 @@ function Quote({quote}) {
     );
 }
 
-async function fetchQuotes(params) {
-    if (!window.navigator.onLine) {
-        console.log('No internet connectivity');
-        return [{
-            text: 'What is the best thing I can do right now ?',
-            author: 'rk'
-        }];
-    }
+async function fetchQuotes() {
     console.log('fetching quotes');
     const response = await fetch('https://gist.githubusercontent.com/ramesh-km/8a1b3a2cce4a8eff58ed83e40c491f35/raw/8f435105e71a6fdf86e97f734145636fc4ae79cd/quotes.json');
     if (response.ok) {
         return await response.json();
     } else {
-        throw new Error('unable to fetch the quotes');
+        throw new Error('for some unknow reason unable to fetch the quotes');
     }
 }
 
